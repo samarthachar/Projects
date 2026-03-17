1530c6616b5c366f67699b286e06eb3d23be153b
Samarth <achar.samarth@gmail.com>
2026-03-17 14:00:00 +0000

    Changes Made: Changed Router Structure

--- CouponManagement/app/main.py
+++ CouponManagement/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI # type: ignore
 
-from app.api.routes import coupons # type : ignore
-from app.api.routes import vouchers # type : ignore
+from app.api.routes.v1.coupons import router as coupons_router 
+from app.api.routes.v1.vouchers import router as vouchers_router 
 
 app = FastAPI(title="Coupons API", version="0.1.0")
 
 
-app.include_router(coupons.router)
-app.include_router(vouchers.router)
+app.include_router(coupons_router)
+app.include_router(vouchers_router)
